10d3940d3ded6bad97c7f454a0a2197630b7aeab
Ruki Fujiwara <rukiwillgotoksf@gmail.com>
2020-05-13 09:58:34 +0900

    bcobu

--- Profile/ViewController.swift
+++ Profile/ViewController.swift
@@ -9,12 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var profileImageView: UIImageView!
+    @IBOutlet var profileCommentLabel: UILabel!
+    @IBOutlet var profileLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func tapButton1() {
+        profileImageView.image = UIImage(named: "phil_image")
+        profileLabel.text = "名前"
+        profileCommentLabel.text = "iPhoneメンターのフィルだよ"
+    }
+    
+    @IBAction func tapButton2() {
+        profileImageView.image = UIImage(named: "track_image")
+        profileLabel.text = "スポーツ"
+        profileCommentLabel.text = "陸上競技が好きで、走り幅跳びが得意"
+    }
+    
+    @IBAction func tapButton3() {
+        profileImageView.image = UIImage(named: "apple_image")
+        profileLabel.text = "好きな食べ物"
+        profileCommentLabel.text = "リンゴが好きで、いつも持ち歩いている"
+    }
+    
+    @IBAction func tapButton4() {
+        profileImageView.image = UIImage(named: "flight_image")
+        profileLabel.text = "趣味"
+        profileCommentLabel.text = "飛行機に乗って、空を散歩すること"
+    }
+    
 }
-
